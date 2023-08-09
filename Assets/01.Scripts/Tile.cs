09c27022ce70762b3698880f09dcb8821822911d
LKM0222 <python03@naver.com>
2023-08-09 23:29:24 +0900

    UISystem 작업 전, Tile기능 추가(Getter, Setter)

--- Assets/01.Scripts/Tile.cs
+++ Assets/01.Scripts/Tile.cs
@@ -6,7 +6,13 @@ public class Tile : MonoBehaviour
 {
     int x,y;
     [SerializeField] int level;
-    int price;
+    [SerializeField] int price;
+    [SerializeField] int rank;
+    [SerializeField] string groundName;
+    [SerializeField] int enforce;
+    [SerializeField] string buildingName;
+    [SerializeField] string buildingType;
+    [SerializeField] string buildingLevel;
 
     GameManager theGM;
 
@@ -20,15 +26,59 @@ public class Tile : MonoBehaviour
         level = _level;
         price = _price;
     }
+
+    //setter
     public void SetLeve(int _level){
         level = _level;
     }
     public void SetSprite(Sprite _sprite){
         this.GetComponent<SpriteRenderer>().sprite = _sprite;
     }
+    public void SetPrice(int _price){
+        price = _price;
+    }
+    public void SetRank(int _rank){
+        rank = _rank;
+    }
+    public void SetGroundName(string _name){
+        groundName = _name;
+    }
+    public void SetEnforce(int _enforce){
+        enforce = _enforce;
+    }
+    public void SetBuildingName(string _BName){
+        buildingName = _BName;
+    }
+    public void SetBuildingType(string _BType){
+        buildingType = _BType;
+    }
+    public void SetBuildingLevel(string _BLevel){
+        buildingLevel = _BLevel;
+    }
 
+    //getter
     public int GetLevel(){
         return level;
     }
-    
+    public int GetPrice(int _price){
+        return price;
+    }
+    public int GetRank(int _rank){
+        return rank;
+    }
+    public string GetGroundName(string _name){
+        return groundName;
+    }
+    public int GetEnforce(int _enforce){
+        return enforce;
+    }
+    public string GetBuildingName(string _BName){
+        return buildingName;
+    }
+    public string GetBuildingType(string _BType){
+        return buildingType;
+    }
+    public string GetBuildingLevel(string _BLevel){
+        return buildingLevel;
+    }
 }
--- Assets/01.Scripts/UISystem.cs
+++ Assets/01.Scripts/UISystem.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISystem : MonoBehaviour
 {
     GameManager theGM;
+    [Header("Tile")]
     [SerializeField] GameObject tilePopupUI;
+    [SerializeField] Text groundRankText;
+    [SerializeField] Text groundPriceText;
+    [SerializeField] Text buildingNameText;
+    [SerializeField] Text buildingTypeText;
+    [SerializeField] Text groundEnforceText;
     // Start is called before the first frame update
     void Start()
     {
